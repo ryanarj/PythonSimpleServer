f865d372e03e94380b834acacae7fb00ecb69d6c
ryanarj <karshan813@gmail.com>
2019-02-11 21:39:51 -0500

    Renamed main to be a seperate class

---
+++ ServerSocket.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+class ServerSocket:
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
+    def __init__(self):
+        soc.bind(('0.0.0.0', 8000))
+        soc.listen(1)
+
+    def targetHandler(self, conn, client_add):
+        while True:
+            data = conn.recv(1024)
+            for item in self.connections:
+                item.send(bytes(data))
+            if not data:
+                break
+
+
+    def start(self):
+        while True:
+            connection, client_address = soc.accept() 
+            thread = threading.Thread(target=self.targetHandler, args=(connection, client_address))
+            thread.daemon = True
+            thread.start()
+            connections.append(connection)
+            print(connections)
+            
